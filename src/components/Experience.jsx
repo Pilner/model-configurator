e0796a2c9ffe13b1c532fe8b7ef1d5081b775298
Railey Macbook Pro <raileyvictuelles@gmail.com>
2025-04-22 07:22:40 +0800

    fix: model clipping through floor

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,44 +1,32 @@
 import { MeshReflectorMaterial, OrbitControls, Stage } from "@react-three/drei";
-
 import { Suspense } from "react";
 import { Umbrella } from "./Umbrella";
 
 export default function Experience() {
 	return (
 		<>
-			<OrbitControls
-				autoRotate={true}
-				autoRotateSpeed={2}
-				maxPolarAngle={Math.PI / 1.75}
-				minDistance={3}
-				target={[0, 2, 0]}
-				position={[0, 2, 5]}
-			/>
-			<Stage
-				environment={"city"}
-				intensity={0.6}
-				contactShadow={false}
-				adjustCamera={false}
-				shadows={false}
-			>
-				<Suspense fallback={null}>
-					<Umbrella />
-				</Suspense>
-			</Stage>
+			<Suspense fallback={null}>
+				<OrbitControls
+					autoRotate={true}
+					autoRotateSpeed={0.5}
+					maxPolarAngle={Math.PI / 1.75}
+					minDistance={3}
+					target={[0, 2, 0]}
+					position={[0, 2, 5]}
+					// enablePan={false}
+				/>
+				<Stage
+					environment={"city"}
+					intensity={0.6}
+					contactShadow={false}
+					adjustCamera={false}
+					shadows={false}
+				/>
+				<Umbrella />
+			</Suspense>
 			<mesh rotation={[-Math.PI / 2, 0, 0]}>
 				<planeGeometry args={[170, 170]} />
-				<MeshReflectorMaterial
-					blur={[100, 100]}
-					resolution={2048}
-					mixBlur={1}
-					mixStrength={40}
-					roughness={1}
-					depthScale={1.2}
-					minDepthThreshold={0.4}
-					maxDepthThreshold={1.4}
-					color={"#101010"}
-					metalness={0.5}
-				/>
+				<MeshReflectorMaterial color={"#101010"} />
 			</mesh>
 		</>
 	);
