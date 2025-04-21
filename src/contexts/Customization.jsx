13cf18cd1f7222ff09214f516b01c14eb1baef0d
Railey Macbook Pro <raileyvictuelles@gmail.com>
2025-04-22 05:54:19 +0800

    feat: implement localStorage to save changes

--- src/contexts/Customization.jsx
+++ src/contexts/Customization.jsx
@@ -4,22 +4,53 @@ import { packages, umbrellaShapes, fabricColors, steelColors } from "../data";
 const CustomizationContext = createContext({});
 
 export function CustomizationProvider(props) {
-	const [packageType, setPackageType] = useState(packages[0]);
-	const [umbrellaShape, setUmbrellaShape] = useState(umbrellaShapes[0]);
-	const [fabricColor, setFabricColor] = useState(fabricColors[0]);
-	const [steelColor, setSteelColor] = useState(steelColors[0]);
+	const [packageType, setPackageType] = useState(() => {
+		const storedPackageType = localStorage.getItem("packageType");
+		return storedPackageType ? JSON.parse(storedPackageType) : packages[0];
+	});
+	const [umbrellaShape, setUmbrellaShape] = useState(() => {
+		const storedUmbrellaShape = localStorage.getItem("umbrellaShape");
+		return storedUmbrellaShape
+			? JSON.parse(storedUmbrellaShape)
+			: umbrellaShapes[0];
+	});
+	const [fabricColor, setFabricColor] = useState(() => {
+		const storedFabricColor = localStorage.getItem("fabricColor");
+		return storedFabricColor
+			? JSON.parse(storedFabricColor)
+			: fabricColors[0];
+	});
+	const [steelColor, setSteelColor] = useState(() => {
+		const storedSteelColor = localStorage.getItem("steelColor");
+		return storedSteelColor ? JSON.parse(storedSteelColor) : steelColors[0];
+	});
 
 	return (
 		<CustomizationContext.Provider
 			value={{
 				packageType,
-				setPackageType,
+				setPackageType: (value) => {
+					setPackageType(value);
+					localStorage.setItem("packageType", JSON.stringify(value));
+				},
 				umbrellaShape,
-				setUmbrellaShape,
+				setUmbrellaShape: (value) => {
+					setUmbrellaShape(value);
+					localStorage.setItem(
+						"umbrellaShape",
+						JSON.stringify(value)
+					);
+				},
 				fabricColor,
-				setFabricColor,
+				setFabricColor: (value) => {
+					setFabricColor(value);
+					localStorage.setItem("fabricColor", JSON.stringify(value));
+				},
 				steelColor,
-				setSteelColor,
+				setSteelColor: (value) => {
+					setSteelColor(value);
+					localStorage.setItem("steelColor", JSON.stringify(value));
+				},
 			}}
 		>
 			{props.children}
